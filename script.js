3b9f04b1c909e7fca7c2b40a44ea8f4855dd4b15
victormaya <victor.maya42@gmail.com>
2020-05-27 19:56:24 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 8
+    msg.innerHTML = `Agora são ${hora} horas.` 
+
+    if (hora >=0 && hora < 12){
+        //bom dia
+        img.src = 'manha.png'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora<18){
+        //tarde
+        img.src = 'tarde.png'
+        document.body.style.background = '#b9846f'
+    } else {
+        //boa noite
+        img.src = 'noite.png'
+        document.body.style.background = '#515154' 
+    }
+
+
+
+
+
+}
+
+
+
